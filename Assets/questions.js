532abfd1c1b96f310a9f01011275854842f5764c
KodaCaleb <122597981+KodaCaleb@users.noreply.github.com>
2023-03-01 05:02:18 -0600

    writing JS for random question functions

--- Assets/questions.js
+++ Assets/questions.js
@@ -5,45 +5,73 @@ q1 =
 {
     id : 1,
     text : "What is Javascript?",
-    a1 : "A technology that allows you to style your HTML elements",
-    a2 : "A computer language that allows you to print text onto certain Coffee",
-    a3 : "An object-oriented computer programming language commonly used to create interactive effects within web browsers.",
-    a4 : "The Entire Bee Movie Script"
+    ca : "An object-oriented computer programming language commonly used to create interactive effects within web browsers.",
+    wa1 : "A technology that allows you to style your HTML elements",
+    wa2 : "A computer language that allows you to print text onto certain Coffee",
+    wa3 : "The Entire Bee Movie Script"
 },
 q2 =
 {
     id: 2,
     text : "Which is NOT a data type in Javascript?",
-    a1 : "String",
-    a2 : "Boolean",
-    a3 : "Object",
-    a4 : "Branch"
+    ca : "Branch",
+    wa1 : "String",
+    wa2 : "Boolean",
+    wa3 : "Object",
 },
 q3 =
 {
     id : 3,
-    text : "What is Javascript?",
-    a1 : "A technology that allows you to style your HTML elements",
-    a2 : "A computer language that allows you to print text onto certain Coffee",
-    a3 : "An object-oriented computer programming language commonly used to create interactive effects within web browsers.",
-    a4 : "The Entire Bee Movie Script"
+    text : "What is the term for a collection of key-value pairs in Javascript?",
+    ca : "Object",
+    wa1 : "Function",
+    wa2 : "Variable",
+    wa3 : "Array"
 },
 q4 = 
 {
     id : 4,
     text : "What is the Syntax to log something to the console?",
-    a1 : "log.console() ",
-    a2 : "console.log()",
-    a3 : "console:log()",
-    a4 : "logTo.Console()"
+    ca : "console.log()",
+    wa1 : "log.console() ",
+    wa2 : "logTo.Console()",
+    wa3 : "console:log()"
 },
 q5 =
 {
     id : 5,
     text : "What is a Function in Javascript?",
-    a1 : "A set of statements that performs a task or calculates a value",
-    a2 : "A way to attach external files into your Javascript",
-    a3 : "A built in feature of javascript that allows you to perform debugging",
-    a4 : "The Entire Bee Movie Script"
-}
+    ca : "A set of statements that performs a task or calculates a value",
+    wa1 : "A way to attach external files into your Javascript",
+    wa2 : "A built in feature of javascript that allows you to perform debugging",
+    wa3 : "The Entire Bee Movie Script"
+},
+q6 = 
+{
+    id : 6,
+    text : "what is NOT a way to declare a variable?",
+    ca : "make",
+    wa1 : "let",
+    wa2 : "const",
+    wa3 : "var"
+},
+q7 = 
+{
+    id : 7,
+    text : "what is NOT a type of loop in Javascript",
+    ca : "for/while",
+    wa1 : "for",
+    wa2 : "while",
+    wa3 : "for/in"
+},
+q8 = 
+{
+    id : 8,
+    text : "What is the term for a type of data in JavaScript that represents true or false values?",
+    ca : "Boolean",
+    wa1 : "String",
+    wa3 : "Number",
+    wa4 : "Object"
+},
+
 ]
