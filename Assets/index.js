71e507158f001bf2257aecedb5d8d9fa1795bfd0
KodaCaleb <122597981+KodaCaleb@users.noreply.github.com>
2023-03-01 02:28:59 -0600

    created question data set and test log function

---
+++ Assets/index.js
@@ -0,0 +1,8 @@
+const nextBtn = $("#nextBtn")
+
+function consoleLog(){
+    console.log(q2)
+}
+
+
+nextBtn.on("click", consoleLog)
---
+++ Assets/questions.js
@@ -0,0 +1,45 @@
+const q1 =
+{
+    id : 1,
+    text : "What is Javascript?",
+    a1 : "A technology that allows you to style your HTML elements",
+    a2 : "A computer language that allows you to print text onto certain Coffee",
+    a3 : "An object-oriented computer programming language commonly used to create interactive effects within web browsers.",
+    a4 : "The Entire Bee Movie Script"
+}
+const q2 =
+{
+    id: 2,
+    text : "Which is NOT a data type in Javascript?",
+    a1 : "String",
+    a2 : "Boolean",
+    a3 : "Object",
+    a4 : "Branch"
+}
+const q3 =
+{
+    id : 3,
+    text : "What is Javascript?",
+    a1 : "A technology that allows you to style your HTML elements",
+    a2 : "A computer language that allows you to print text onto certain Coffee",
+    a3 : "An object-oriented computer programming language commonly used to create interactive effects within web browsers.",
+    a4 : "The Entire Bee Movie Script"
+}
+const q4 = 
+{
+    id : 4,
+    text : "What is the Syntax to log something to the console?",
+    a1 : "log.console() ",
+    a2 : "console.log()",
+    a3 : "console:log()",
+    a4 : "logTo.Console()"
+}
+const q5 =
+{
+    id : 5,
+    text : "What is a Function in Javascript?",
+    a1 : "A set of statements that performs a task or calculates a value",
+    a2 : "A way to attach external files into your Javascript",
+    a3 : "A built in feature of javascript that allows you to perform debugging",
+    a4 : "The Entire Bee Movie Script"
+}
